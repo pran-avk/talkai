7ef3932c0bb4a422cdfda5a87ef0c8f84e6fa332
Pranav <kp755505@gmail.com>
2025-05-04 13:16:43 +0530

    server commit2

--- core/settings.py
+++ core/settings.py
@@ -116,7 +116,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 import os
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
